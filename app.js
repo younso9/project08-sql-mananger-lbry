962f2df93b3e7707720a2a0ef1fdaefb5fb23fe9
younso9 <sonya.youngblood@verizon.com>
2019-08-26 13:31:40 -0400

    Pug Files

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("Hello Sage");
+
+const express = require('express');
+const app = express();
+
+const portNumber = 3000;
+app.listen(portNumber);
+console.log("App started on localhost at port " + portNumber);
+
+
+
+
