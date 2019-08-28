cb63b4f484f0e966ccd8a6ad4cd9f9166dc1c5c8
younso9 <sonya.youngblood@verizon.com>
2019-08-28 13:46:02 -0400

    commit

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-//importing the express app established in routes/routes.js
+//Importing the express app established in routes/routes.js
 const app = require('./routes/routes');
 
-//apply our pug template
+//Apply our pug template
 const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//establish our server
+
+//The End - establish our server
 const portNumber = 3000;
 app.listen(portNumber);
 console.log("App started on localhost at port " + portNumber);
-
-
-
-
